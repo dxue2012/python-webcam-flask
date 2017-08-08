a27d35f841614534bac1b5d2a79177677ca48757
Dyland Xue <dylandxue2012@gmail.com>
2017-08-07 22:52:14 -0400

    flip image

--- makeup_artist.py
+++ makeup_artist.py
@@ -1,6 +1,9 @@
+from PIL import Image
+
+
 class Makeup_artist(object):
     def __init__(self):
         pass
 
     def apply_makeup(self, img):
-        return img
+        return img.transpose(Image.FLIP_LEFT_RIGHT)
